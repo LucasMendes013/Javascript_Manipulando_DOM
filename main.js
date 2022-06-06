978bbf9340a13510d35a3646ead04e433d6b93d3
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-06 10:56:12 -0300

    main

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ const robotron = document.querySelector('#robotron')
 
 robotron.addEventListener('click', dizerOi)
 
-function dizerOi(){
-    console.log('oi')
+function dizerOi(nome){
+    console.log(`Ola ${nome}`)
 }
+
+//formas de declarar uma function com dom
+
+// robotron.addEventListener('click', () => {
+//     console.log("Uma Arial Function")
+// })
+
+// robotron.addEventListener('click', function(){
+//     console.log("Uma funcao anonima, nao podemos chamala novamente pois nao tem nome")
+// })
+
+// podemos chamar a funcao com o nome dela 
+// dizerOi()
