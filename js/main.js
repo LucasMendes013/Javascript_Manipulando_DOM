0afb2257e4e33dd6a91a668b8692f6ddd7fef898
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-07 08:25:53 -0300

    com

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
-const controle = document.querySelectorAll(".controle-ajuste")
+const controle = document.querySelector(".controle-ajuste")
 
-somar.addEventListener("click", () => {manipulaDados("somar")})
-
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+controle.forEach((elemento) => { 
+    elemento.addEventListener("click", () => {
+        console.log(evento.target)
+    })
+})
 
 function manipulaDados(operacao) {
-    if(operacao === "subtrair") {
+    if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1
     } else {
         braco.value = parseInt(braco.value) + 1
